0eaee55fe205df9a3465b9d32b9cc4bdfe146c5c
RaviTeja <bethuraviteja.in@gmail.com>
2026-03-11 23:06:20 +0530

    urls update

---
+++ store/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('products/', views.product_list),
+
+    path('products/<int:id>/', views.product_detail),
+
+    path('collections/', views.collection_list),
+
+    path('collections-products/', views.collections_products),
+
+    path('collections-count/', views.collections_with_product_count),
+
+    path('values-products/', views.product_titles_prices),
+
+    path('valueslist-products/', views.product_titles_prices_list),
+
+    path('cheap-products/', views.cheap_or_high_inventory),
+
+    path('increase-inventory/', views.increase_inventory),
+]

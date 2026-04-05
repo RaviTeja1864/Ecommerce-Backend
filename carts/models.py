9035180a78856fb95cbe30c9431987f094501580
RaviTeja <bethuraviteja.in@gmail.com>
2026-04-06 00:32:03 +0530

    cart model implementation

--- carts/models.py
+++ carts/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from store.models import Product
-
+import uuid
+from django.core.validators import MinValueValidator
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
-        return f"Cart{self.id}"
+        return str(self.id)
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart , on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
 
     class Meta:
         unique_together = ['cart','product']
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.title}"

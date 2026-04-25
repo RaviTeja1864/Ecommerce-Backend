b50e28c9c63f2f9981d0387a0ca10a185b8f58b1
RaviTeja <bethuraviteja.in@gmail.com>
2026-04-25 22:44:24 +0530

    serializers

--- carts/serializers.py
+++ carts/serializers.py
@@ -1,1 +1,33 @@
 from rest_framework import serializers
+
+class AddCarItemSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField()
+
+
+    def validate_product_id(self,value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError("Product does not exist")
+        return value
+
+        def save(self, **kwargs):
+            cart_id = self.context['cart_id']
+            product_id = self.validated_data['product_id']
+            quantity = self.validated_data['quantity']
+
+            try:
+                cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+                cart_item.quantity += quantity
+                cart_item.save()
+                sef.instance = cart_item
+
+            except CartItem.DoesNotExist:
+                self.instance = CartItem.objects.create(cart_id=cart_id, product_id=product_id, quantity=quantity)
+            
+            return self.instance
+    class Meta:
+        model = CartItem
+        fields = ['id' , 'product_id' , 'quantity']
+    
+    
+
+        

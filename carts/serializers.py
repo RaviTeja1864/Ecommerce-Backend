f13cd259ae7e4e37197695ad2753c7b07b08f568
RaviTeja <bethuraviteja.in@gmail.com>
2026-04-29 21:45:06 +0530

    updating cart serializers

--- carts/serializers.py
+++ carts/serializers.py
@@ -1,33 +1,31 @@
 from rest_framework import serializers
+from store.models import Product
+from .models import Cart, CartItem
 
-class AddCarItemSerializer(serializers.Serializer):
-    product_id = serializers.IntegerField()
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price']
 
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField()
 
-    def validate_product_id(self,value):
-        if not Product.objects.filter(pk=value).exists():
-            raise serializers.ValidationError("Product does not exist")
-        return value
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.price
 
-        def save(self, **kwargs):
-            cart_id = self.context['cart_id']
-            product_id = self.validated_data['product_id']
-            quantity = self.validated_data['quantity']
-
-            try:
-                cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
-                cart_item.quantity += quantity
-                cart_item.save()
-                sef.instance = cart_item
-
-            except CartItem.DoesNotExist:
-                self.instance = CartItem.objects.create(cart_id=cart_id, product_id=product_id, quantity=quantity)
-            
-            return self.instance
     class Meta:
         model = CartItem
-        fields = ['id' , 'product_id' , 'quantity']
-    
-    
+        fields = ['id', 'product', 'quantity', 'total_price']
 
-        
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True, read_only=True, source='cartitem_set')
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart: Cart):
+        return sum([item.quantity * item.product.price for item in cart.cartitem_set.all()])
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'items', 'total_price']

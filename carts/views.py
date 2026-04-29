af0d99c8448dabbeda4dd79d14f2c5472994e753
RaviTeja <bethuraviteja.in@gmail.com>
2026-04-29 23:28:05 +0530

    optimizatizing based on cart serializers

--- carts/views.py
+++ carts/views.py
@@ -1,2 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from .models import Cart
+from .serializers import CartSerializer
 
+class CartViewSet(CreateModelMixin, 
+                  RetrieveModelMixin, 
+                  DestroyModelMixin, 
+                  GenericViewSet):
+    
+    queryset = Cart.objects.prefetch_related('cartitem_set__product').all()
+    serializer_class = CartSerializer

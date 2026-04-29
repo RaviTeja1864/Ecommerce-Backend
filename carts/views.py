30738b5baf70664c5b9d9e39b223c83c88761df6
RaviTeja <bethuraviteja.in@gmail.com>
2026-04-30 01:32:50 +0530

    updating views

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
-from .models import Cart
-from .serializers import CartSerializer
+from .models import Cart, CartItem
+from .serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 
 class CartViewSet(CreateModelMixin, 
                   RetrieveModelMixin, 
@@ -10,3 +10,19 @@ class CartViewSet(CreateModelMixin,
     
     queryset = Cart.objects.prefetch_related('cartitem_set__product').all()
     serializer_class = CartSerializer
+
+class CartItemViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
